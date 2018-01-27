75634f04a825794d3cbbca765110ed3d646f8a4f
Fernando Renesto <fernandorenesto@Fernandos-MacBook-Pro.local>
2018-01-27 03:36:27 -0200

    Setup ready

--- prototipo1/gruntfile.js
+++ prototipo1/gruntfile.js
@@ -5,37 +5,76 @@ module.exports = function(grunt) {
       files: ['build/js/*.js'],
     },
     sass: {
-      dist:{
+      bethaindex:{
         files: [{
-          'app/index.css' : 'build/sass/*.scss'
+          'betha/css/index.css' : 'build/sass/index.scss'
         }]
-      }
+      },
     },
     copy:{
-      files:[
-        {expand: true, src: ['build/images/*'], dest: 'app/images/'}]
+      html: {
+        expand: true,
+        cwd:'build/html/',
+        src: '*',
+        dest: 'betha/',
+        filter:'isFile'
+      },
+      manifest:{
+        expand: true,
+        cwd:'build/',
+        src: 'manifest.json',
+        dest: 'betha/',
+        filter:'isFile'
+      }
+    },
+    concat:{
+      index:{
+        src: ['build/js/*.js'],
+        dest: 'betha/js/index.js'
+      },
+      bethalibs:{
+        src: ['build/js/libs/*.js'],
+        dest: 'betha/js/libs.js'
+      }
+    },
+    imagemin:{
+      bethaimg:{
+        files: [{
+            expand: true,
+            cwd: 'build/images/',
+            src: ['*.{png,jpg,gif}'],
+            dest: 'betha/images/'
+        }]
+      }
     },
     uglify:{
       my_target: {
         files: {
-          'app/index.js': ['build/js/libs/*.js','build/js/*.js']
+          'betha/index.js': ['build/js/libs/*.js','build/js/*.js']
         }
       }
     },
     watch: {
-      files: ['build/js/*.js','build/sass/*.scss'],
-      tasks: ['jshint','sass']
+      files: [
+        'build/js/**/*.js',
+        'build/sass/**/*.scss',
+        'build/images/*.{png,jpg,gif}',
+        'build/html/*.html'
+       ],
+      tasks: ['jshint','sass','concat','copy']
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('build-test', ['jshint','sass','copy','watch']);
-
-  grunt.registerTask('build-prod', ['sass','uglify']);
+  grunt.registerTask('default',['jshint','sass','concat','imagemin','watch']);
+  grunt.registerTask('init-project',['jshint','sass','copy','concat','imagemin']);
+  //grunt.registerTask('build', ['sass','uglify']);
 
 };
